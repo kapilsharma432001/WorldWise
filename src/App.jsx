7e597ed56593f7f113f5bb8c596eb60ccae51d1c
nxg06062 <kapil.sharma_3@nxp.com>
2024-06-26 12:25:41 +0530

    Mistankely added 'Starter' folder that has been removed now, also added some starter css and jsx files in components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
