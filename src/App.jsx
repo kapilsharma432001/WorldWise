50a4d931b71d273941e2687c20e797a5826ab785
kapilsharma432001 <sharmakapil432001@gmail.com>
2024-06-28 14:10:57 +0530

    Storing state in the URL for question id and lan, lat

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useState, useEffect } from "react";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -47,7 +49,11 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="login" element={<Login />} />
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,10 @@
 import CityItem from "./CityItem";
+import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
